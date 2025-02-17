b64f968e2b5434681c26a1fdffac7f12d99dd8c2
canyo <canyoldas5410@gmail.com>
2025-02-17 21:11:59 +0300

    2.9_@model Direktifi kodlandı

--- Basics/Controllers/EmployeeController.cs
+++ Basics/Controllers/EmployeeController.cs
@@ -1,21 +1,55 @@
+using Basics.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basics.Controllers;
 
 public class EmployeeController : Controller
 {
-    public string Index()
+    public IActionResult Index1()
     {
-        return "Hello, World!";
+        string message = $"Hi! {DateTime.Now.ToString()}";
+        return View("Index1", message);
     }
 
     public ViewResult Index2()
     {
-        return View("Index");
+        var names = new string[]
+        {
+            "Ahmet",
+            "Mehmet",
+            "Can"
+        };
+        
+        return View(names);
     }
 
     public IActionResult Index3()
     {
-        return Content("Employee");
+        var list = new List<Employee>()
+        {
+            new Employee()
+            {
+                Id = 1,
+                FirstName = "Ahmet",
+                LastName = "Can",
+                Age = 20
+            },
+            new Employee()
+            {
+                Id = 2,
+                FirstName = "Okan",
+                LastName = "Can",
+                Age = 30
+            },
+            new Employee()
+            {
+                Id = 3,
+                FirstName = "Berk",
+                LastName = "Can",
+                Age = 40
+            }
+        };
+
+        return View("Index3", list);
     }
 }
